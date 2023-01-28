a45b5c4cdd30d464dffd09320b9771140ea17ed2
danielocampo226 <sierradanny562@gmail.com>
2023-01-28 12:37:13 -0500

    method overloading

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace MethodOverloading
+﻿using System.Transactions;
+
+namespace MethodOverloading
 {
-    public class Program
+    public class Methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            var answer = Methods.Add(2, 2, true);
+            Console.WriteLine(answer);
+                       
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+
+        public static string Add(int num1, int num2, bool isTrue)
+        {
+            var sum = num1 + num2;
+            if (isTrue && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+
+            else if (isTrue && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else
+            {
+                return sum.ToString();
+            }
         }
     }
+
 }
